e14c05f746b850cf69ce81b69d6be4b9786ee1c5
DevExpressExampleBot <fakeEmail>
2021-09-02 19:25:40 +0300

    Source auto update [skip ci]

--- VB/MapControl_CustomGeocodePanel/MainWindow.xaml.vb
+++ VB/MapControl_CustomGeocodePanel/MainWindow.xaml.vb
@@ -4,32 +4,32 @@ Imports System.Globalization
 Imports System.Windows
 
 Namespace MapControl_CustomGeocodePanel
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        #Region "#SearchButtonClick"
-        Private privateGeocodeTarget As GeoPoint
-        Public Property GeocodeTarget() As GeoPoint
-            Get
-                Return privateGeocodeTarget
-            End Get
-            Private Set(ByVal value As GeoPoint)
-                privateGeocodeTarget = value
-            End Set
-        End Property
+		#Region "#SearchButtonClick"
+		Private privateGeocodeTarget As GeoPoint
+		Public Property GeocodeTarget() As GeoPoint
+			Get
+				Return privateGeocodeTarget
+			End Get
+			Private Set(ByVal value As GeoPoint)
+				privateGeocodeTarget = value
+			End Set
+		End Property
 
-        Public Sub New()
-            InitializeComponent()
-            GeocodeTarget = New GeoPoint()
-            DataContext = GeocodeTarget
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			GeocodeTarget = New GeoPoint()
+			DataContext = GeocodeTarget
+		End Sub
 
-        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            geocodeProvider.RequestLocationInformation(GeocodeTarget, Nothing)
-        End Sub
-        #End Region ' #SearchButtonClick
-    End Class
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			geocodeProvider.RequestLocationInformation(GeocodeTarget, Nothing)
+		End Sub
+		#End Region ' #SearchButtonClick
+	End Class
 End Namespace
